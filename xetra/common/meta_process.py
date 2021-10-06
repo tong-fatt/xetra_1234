5d85aaf795daba1f19cc5af49e0f842eb102d015
tho662003 <tho662003@yahoo.com>
2021-10-06 18:47:02 +0800

    added list_files_in_prefix and unittests

---
+++ xetra/common/meta_process.py
@@ -0,0 +1,18 @@
+"""
+Methods for processing the meta file
+"""
+
+class MetaProcess():
+    """
+    class for working with meta file
+    """
+
+
+    @staticmethod
+    def update_meta_file():
+        pass
+
+
+    @staticmethod
+    def return_meta_list():
+        pass
